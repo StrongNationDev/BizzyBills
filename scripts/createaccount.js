a75a06ea73a65f4bd4027d4ff38e6d034be45463
PrinceKes <princeade5565@gmail.com>
2025-07-02 13:23:08 -0700

    update all files

--- scripts/createaccount.js
+++ scripts/createaccount.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://xhuyzhlutarpffhdwbni.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // Keep your full key here
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhodXl6aGx1dGFycGZmaGR3Ym5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2MDM0NDYsImV4cCI6MjA2NTE3OTQ0Nn0.upEAFWSU9GD5-qLwHtuV2eb9yHKEFs_JTaN-quymXaM';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 document.getElementById('registerForm').addEventListener('submit', async (e) => {
