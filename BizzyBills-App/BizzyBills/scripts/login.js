4ebe6aea53e3a8142b6dcf52c983c18994edac52
Enoch | StrongNationDev <96037694+StrongNationDev@users.noreply.github.com>
2025-12-11 12:33:46 +0100

    Update Supabase client import to version 2.43.4

--- BizzyBills-App/BizzyBills/scripts/login.js
+++ BizzyBills-App/BizzyBills/scripts/login.js
@@ -1,5 +1,8 @@
 // scripts/login.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+//import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.43.4/+esm";
+
 
 const supabaseUrl = 'https://xhuyzhlutarpffhdwbni.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhodXl6aGx1dGFycGZmaGR3Ym5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2MDM0NDYsImV4cCI6MjA2NTE3OTQ0Nn0.upEAFWSU9GD5-qLwHtuV2eb9yHKEFs_JTaN-quymXaM';
